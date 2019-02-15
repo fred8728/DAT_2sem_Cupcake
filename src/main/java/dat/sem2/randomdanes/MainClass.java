f5a9676047a661414d57318d9813d224e3416d39
fred8728 <fskn@hotmail.com>
2019-02-15 12:25:53 +0100

    simone2

--- src/main/java/dat/sem2/randomdanes/MainClass.java
+++ src/main/java/dat/sem2/randomdanes/MainClass.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class MainClass {
     
     public static void main( String[] args ) {
-        System.out.println("Det simones fil");
+        System.out.println("Det simones fil nr 2");
         int numberOfDanes = 20; // default 
         if ( args.length > 0 ) {
             try {numberOfDanes = Integer.parseInt( args[0]);} 
