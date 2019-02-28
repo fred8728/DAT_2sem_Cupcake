c4e6f3d2069af3cb7e2c115fc57ad4ded2461cf3
fred8728 <fskn@hotmail.com>
2019-02-28 12:01:18 +0100

    simone

--- exercise1recipes/src/main/java/Main.java
+++ exercise1recipes/src/main/java/Main.java
@@ -25,7 +25,7 @@ public class Main {
                 + "values "
                 + "('" + "8" + "', '" + "Lakselasagne" + "');";
         statement.executeUpdate(query);
-        */
+        
         
         Recipe rep = accessor.getRecipe(2);
         
@@ -37,7 +37,7 @@ public class Main {
         System.out.println(rep.toString());
         System.out.println(rep.toString());
         System.out.println(rep.toString());
-        
+        */
         
     }
 }
--- exercise1recipes/src/main/java/lol.java
+++ exercise1recipes/src/main/java/lol.java
@@ -55,8 +55,8 @@ public class lol extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    /*protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+           throws ServletException, IOException {
         ArrayList<String> list = new ArrayList<>();
         Data_access accessor = new Data_access();
         ArrayList<Integer> num = new ArrayList<>();
@@ -79,7 +79,7 @@ public class lol extends HttpServlet {
                 }
             } 
 
- /* TODO output your page here. You may use following sample code. */
+  TODO output your page here. You may use following sample code. 
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
@@ -100,7 +100,7 @@ public class lol extends HttpServlet {
             out.println("</html>");
         }
     }
-
+*/
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -110,12 +110,13 @@ public class lol extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    /*
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
+*/
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -124,20 +125,20 @@ public class lol extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
+    /*@Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
-    }
+    }*/
 
     /**
      * Returns a short description of the servlet.
      *
      * @return a String containing servlet description
      */
-    @Override
+    /* @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
+*/
 }
