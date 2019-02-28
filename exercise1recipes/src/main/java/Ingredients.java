153997bc31007db04432178c3c1d450ce31047c0
Emil-Skovbo <emilskovbo18@gmail.com>
2019-02-28 10:36:58 +0100

    2nd try

---
+++ exercise1recipes/src/main/java/Ingredients.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author frizz
+ */
+public class Ingredients {
+    
+    private int id_ingre;
+    private int id_recipe;
+
+    public Ingredients(int id_ingre, int id_recipe) {
+        this.id_ingre = id_ingre;
+        this.id_recipe = id_recipe;
+    }
+    
+}
