7e47c0f8ac464dffee7c2c0a3b5e4e611d0d63d7
Fred <44893926+FrederikLL@users.noreply.github.com>
2019-02-28 12:20:59 +0100

    Fixed better

--- exercise1recipes/src/main/java/User.java
+++ exercise1recipes/src/main/java/User.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor.twttwr
  */
 
 /**
@@ -21,7 +21,7 @@ public class User {
         this.password = password;
         this.balance = balance;
         
-    } 
+    }
     
     public String toString (){
         return "User: User_ID = " + id + "Username = " + username + "Password = " + password + "Balance = " + balance; 
