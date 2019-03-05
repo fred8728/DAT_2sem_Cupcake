c271cbd11b88b578c9587c36c4fa6626839e3c49
Fred <44893926+FrederikLL@users.noreply.github.com>
2019-03-05 11:28:06 +0100

    product control og line og shopping class

--- exercise1recipes/src/main/java/ShoppingCart.java
+++ exercise1recipes/src/main/java/ShoppingCart.java
@@ -1,3 +1,7 @@
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +13,10 @@
  * @author frizz
  */
 public class ShoppingCart {
+    Data_access accessor = new Data_access();
+    //LineItems lineQ = new LineItems();
+    
+    //List orders = 
+
     
 }
---
+++ exercise1recipes/src/main/webapp/ProductControl.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : ProductControl
+    Created on : 05-03-2019, 11:27:27
+    Author     : frizz
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
