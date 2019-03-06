9c7a6f74fe5a8098e9776eb9c5199e0003377cc3
Fred <44893926+FrederikLL@users.noreply.github.com>
2019-03-06 11:39:48 +0100

    fix

--- exercise1recipes/src/main/java/PageOrder.java
+++ exercise1recipes/src/main/java/PageOrder.java
@@ -19,7 +19,7 @@ public class PageOrder {
 
         Data_access acc = new Data_access();
         LineItems liner = null;
-        ShoppingCart shopper = null;
+        ShoppingCart shopperz = new ShoppingCart();
         String username = request.getParameter("bottom");
         String password = request.getParameter("topping");
         String quant = request.getParameter("qty");
@@ -27,7 +27,7 @@ public class PageOrder {
         int top = Integer.parseInt(password);
         int quanti = Integer.parseInt(quant);
         liner = new LineItems(top, bot, quanti);
-        shopper.addToList(liner);
+        shopperz.addToList(liner);
         try {
             acc.sendOrderToDB(bot, top, quanti);
         } catch (Exception e) {
